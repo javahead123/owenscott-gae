1e3f0402f2fb97ee07bc3e5401dfec5c573fcdd7
Owen Martin <owen@altosresearch.com>
2013-06-06 13:46:45 -0700

    Complete Chapter 3

--- main.py
+++ main.py
@@ -33,10 +33,6 @@ PASSWORD_RE = re.compile("^.{3,20}$")
 EMAIL_RE = re.compile("^[\S]+@[\S]+\.[\S]+$")
 
 
-class BlogPost(db.Model):
-  title = db.StringProperty(required=True)
-  content = db.TextProperty(required=True)
-  created = db.DateTimeProperty(auto_now_add=True)
 
 
 class Handler(webapp2.RequestHandler):
@@ -50,42 +46,77 @@ class Handler(webapp2.RequestHandler):
   def render(self, template, **kwargs):
     self.write(self.render_str(template, **kwargs))
 
-
 class MainHandler(Handler):
+  def get(self):
+    self.render("mainpage.html")
+
+
+
+
+###### Database considerations:
+
+def blog_key(name = "default"):
+  return db.Key.from_path('blogs', name)
+
+class BlogPost(db.Model):
+  subject = db.StringProperty(required=True)
+  content = db.TextProperty(required=True)
+  created = db.DateTimeProperty(auto_now_add=True)
+  last_modified = db.DateTimeProperty(auto_now_add=True)
+
+########
+
+####### Blog Handlers
+
+class BlogHandler(Handler):
   def get(self):
     posts = db.GqlQuery("SELECT * FROM BlogPost ORDER BY created DESC LIMIT 10") # Note: because class name is BlogPost
     self.render("mainpage.html", posts = posts)
 
+
+
+class BlogPermalink(Handler):
+  def get(self, post_id):
+    key = db.Key.from_path('BlogPost', int(post_id))
+    post_data = db.get(key)
+    if not post_data:
+      self.error(404)
+    self.render("mainpage.html", posts = [post_data])
+
+
+
+
+
 class NewPostHandler(Handler):
-  def render_form(self, title = "", content = "", error = ""):
-    self.render("form.html", title = title, content = content, error = error)
+  def render_form(self, subject = "", content = "", error = ""):
+    self.render("form.html", subject = subject, content = content, error = error)
   def get(self):
     self.render_form()
 
   def post(self):
-    title = self.request.get('title')
+    subject = self.request.get('subject')
     content = self.request.get('content')
 
-    if title and content:
-      a = BlogPost(title = title, content = content)
+    if subject and content:
+      a = BlogPost(subject = subject, content = content)
       a.put()
-      self.redirect("/")
-
+      self.redirect("/blog/%s" % str(a.key().id()))
     else:
-      self.render_form(title = title, content = content, error = "We need both a Title and some Content!")
+      self.render_form(subject = subject, content = content, error = "We need both a subject and some Content!")
 
-class PostHandler(Handler):
-  def get(self):
-    self.request
-    self.render("mainpage.html")
+
+
+
+
 
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/post/(.*)', PostHandler),
-    ('/newpost', NewPostHandler)], debug=True)
+    ('/blog/?', BlogHandler),
+    ('/blog/([0-9]+)', BlogPermalink),
+    ('/blog/newpost', NewPostHandler)], debug=True)
 
 ### Spec:
 # '/' => point to blog, display most recent entries
-# '/newpost' => points to a form with title, content--- sends back error unless both are filled out
+# '/newpost' => points to a form with subject, content--- sends back error unless both are filled out
 # a successful submission creates a permalink page with that entry
